a96534fa1d8144a15cc10696a17237894cd40662
Vinay2603 <91530880+Vinay2603@users.noreply.github.com>
2021-12-13 17:36:24 +0530

    add

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,23 +1,31 @@
 const express = require("express")
 const User = require("../modules/user")
 const upload = require("../middleware/upload")
+const protect = require("../middleware/protect")
 const router = express.Router();
 
-router.post("/" ,upload.single("profile_photo_url") , async(req,res)=>{
-    console.log(req)
+router.post("/" ,upload.single("profile_photo_url"), async(req,res)=>{
+   
     try{
        const user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password ,
         profile_photo_url:req.file.path,
-       // roles:req.body.roles 
+        roles:req.body.roles 
        })
        return res.status(201).send({user})
     }catch(e){
         return res.status(500).json({message : e.message , status: "failed"})
     }
 })
+
+router.get("/",protect, async(req,res)=>{
+    const user = await User.find({}).select("-password").lean().exec()
+    return res.status(201).send(user)
+})
+
+
 
 
 module.exports = router ;
